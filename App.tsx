a8489745d403f305634832ac1674e4bab156ae54
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-20 13:46:35 -0500

    Use State Hook

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   Button,
@@ -20,11 +20,20 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const onClickHandler = () => {
+    setCount(count + 1);
+    setNumber(number + 5);
+  }
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming</Text>
-      <Button title="google link"
-        onPress={() => Linking.openURL("http://google.com")}></Button>
+      <Text style={styles.text}>{number}</Text>
+      <Button title="Update State"
+        onPress={onClickHandler}></Button>
+      <Text style={styles.text}>You clicked {count} {count > 1 ? "times" : "time"}</Text>
     </View>
   );
 }
