6bc3c7f5e297c31c57ba99917bd776b776e6cacf
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-20 16:00:18 -0500

    Alert, Toast

--- App.tsx
+++ App.tsx
@@ -5,9 +5,10 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import {
+  Alert,
   Button,
   FlatList,
   Linking,
@@ -18,30 +19,55 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  ToastAndroid,
   TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): JSX.Element {
-  
   const [name, setName] = useState('');
   const [submitted, setSubmitted] = useState(false);
   const onPressHandler = () => {
-    setSubmitted(!submitted);
-  }
+    if (name.length > 3) {
+      setSubmitted(!submitted);
+    } else {
+      // Alert.alert(
+      //   'Warning',
+      //   'The name must be longer than 3 characters',
+      //   [
+      //     {
+      //       text: 'Do not show again',
+      //       onPress: () => console.warn('Do not show again!'),
+      //       style: 'destructive',
+      //     },
+      //     {
+      //       text: 'Cancel',
+      //       onPress: () => console.warn('Cancel Pressed!'),
+      //       style: 'destructive',
+      //     },
+      //     {
+      //       text: 'OK',
+      //       onPress: () => console.warn('OK Pressed!'),
+      //       style: 'destructive',
+      //     },
+      //   ],
+      //   {cancelable: true, onDismiss: () => console.warn('Alert dismissed!')},
+      // );
+
+      ToastAndroid.show('The name must be longer than 3 characters',
+        ToastAndroid.SHORT)
+    }
+  };
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>
-        Please write your name:
-      </Text>
-      <TextInput style={styles.input}
-        placeholder='e.g. John'
-        onChangeText={(value) => setName(value)}
+      <Text style={styles.text}>Please write your name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. John"
+        onChangeText={value => setName(value)}
       />
       {/* <Button title={submitted ? 'Clear' : 'Submit'}
         onPress={onPressHandler}
@@ -51,27 +77,15 @@ function App(): JSX.Element {
       <TouchableOpacity
         style={styles.button}
         onPress={onPressHandler}
-        activeOpacity={0.2}
-      >
-        <Text style={styles.text}>
-        {submitted ? 'Clear' : 'Submit'}
-        </Text>
+        activeOpacity={0.2}>
+        <Text style={styles.text}>{submitted ? 'Clear' : 'Submit'}</Text>
       </TouchableOpacity>
-      <Pressable
-        style={styles.button}
-        onPress={onPressHandler}
-      >
-        <Text style={styles.text}>
-          {submitted ? 'Clear' : 'Submit'}
-        </Text>
+      <Pressable style={styles.button} onPress={onPressHandler}>
+        <Text style={styles.text}>{submitted ? 'Clear' : 'Submit'}</Text>
       </Pressable>
-      {submitted ? 
-        <Text style={styles.text}>
-          You are registered as {name}
-        </Text>
-        :
-        null
-      }
+      {submitted ? (
+        <Text style={styles.text}>You are registered as {name}</Text>
+      ) : null}
     </View>
   );
 }
@@ -85,7 +99,7 @@ const styles = StyleSheet.create({
   text: {
     color: '#000',
     fontSize: 20,
-    margin: 10
+    margin: 10,
   },
   input: {
     width: 200,
@@ -101,7 +115,7 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: '#00ff00',
     alignItems: 'center',
-  }
+  },
 });
 
 export default App;
