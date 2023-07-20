6e37def65603c35b0e57725e9573a88f7c0d2412
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-20 14:10:33 -0500

    flex

--- App.tsx
+++ App.tsx
@@ -28,10 +28,14 @@ function App(): JSX.Element {
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <View style={styles.button}>
-        <Button title="Update State"
-          onPress={onClickHandler}></Button>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
       </View>
     </View>
   );
@@ -40,24 +44,36 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view1: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    width: 100,
+    height: 100,
     backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 10,
-    borderColor: '#ff00ff',
-    borderRadius: 10,
-    margin: 40
   },
   text: {
     color: '#000',
     fontSize: 20,
     fontStyle: 'italic',
-    margin: 10,
-    textTransform: 'uppercase',
-  },
-  button: {
-    width: 200,
-    height: 60,
+    margin: 10
   }
 });
 
