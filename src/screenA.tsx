448949afd027dfc0e0f28186987b853c910946d2
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-07-20 18:42:39 -0500

    Tab navigation

---
+++ src/screenA.tsx
@@ -0,0 +1,34 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+
+const ScreenA: React.FC = ({navigation}) => {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+    //navigation.replace('Screen_B');
+  };
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({pressed}) => ({backgroundcolor: pressed ? '#ddd' : '#0f0'})}>
+        <Text style={styles.text}>Go to Screen B</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+});
+
+export default ScreenA;
